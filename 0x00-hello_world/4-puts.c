03b3aa7fff5802317ff9ac3355e69ed1e1e22f7c
Meriem1551 <meriem200409@gmail.com>
2023-09-07 03:51:25 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
-int main(){
-	puts("Programming is like building a multilingual puzzle \n");
+int main(void){
+	puts("/"Programming is like building a multilingual puzzle");
 	return (0);
 }
