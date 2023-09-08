46eb100ccccabba75d79ac6ff145ef51ec2dc27c
Meriem1551 <meriem200409@gmail.com>
2023-09-08 09:43:11 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - printing numbers from 0-9 with commas and space between them
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
+ */
+int main(void)
+{
+int d1;
+int d2;
+int num;
+
+for (d1 = 0 ; d1 <=8 ; d1++)
+{
+for (d2 = d1 + 1 ; d2 <= 9 ; d2++)
+{
+num = d1 * 10 + d2;
+putchar(num + '0');
+if ( num != 89)
+{
+putchar(',');
+putcar(' ');
+}
+}
+putchar ('\n');
+return (0);
+}
