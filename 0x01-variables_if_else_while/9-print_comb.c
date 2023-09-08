7789717c5b16411b872d6696489a16fb441b8ffd
Meriem1551 <meriem200409@gmail.com>
2023-09-08 09:08:52 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+
+for (num = 0 ; num <= 9 ; num++)
+{
+putchar(num + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
