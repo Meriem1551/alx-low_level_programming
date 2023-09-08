8580ceb629c089d4527a5186edc241619bec454e
Meriem1551 <meriem200409@gmail.com>
2023-09-08 13:10:05 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - program that prints all possible different combinations of 3 digits.
- * Return: 0
- */
+* main - program that prints all possible different combinations of 3 digits.
+* Return: 0
+*/
 int main(void)
 {
  int i, j;
