c9a58dc007a946b7df09a507cbf9ddc81fa02ff9
Meriem1551 <meriem200409@gmail.com>
2023-09-08 12:50:46 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
-int num=0;
+int num = 0;
 int d1;
 int d2;
 int d3;
 
-while (num <=999)
+while (num <= 999)
 {
 d1 = (num / 100 + '0');
-d2= (num / 10 % 10 + '0');
+d2 = (num / 10 % 10 + '0');
 d3 = (num % 10 + '0');
-if ((d1 < d2) && (d2 < d3))
+if ((d1 < d2) &&(d2 < d3))
 {
 putchar(d1);
 putchar(d2);
