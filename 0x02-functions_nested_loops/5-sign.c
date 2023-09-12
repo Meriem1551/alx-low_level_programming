29829ac1c0b2a56a5925ff67080fdf836363bebc
Meriem1551 <meriem200409@gmail.com>
2023-09-12 04:27:21 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-return (1);
 _putchar('+');
+return (1);
 }
 else if (n == 0)
 {
-return (0);
 _putchar('0');
+return (0);
 }
 else
 {
-return (-1);
 _putchar('-');
+return (-1);
 }
 }
