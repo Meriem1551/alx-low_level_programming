85b387fea2197066f0eb2a936f02f92e8434c25c
Meriem1551 <meriem200409@gmail.com>
2023-09-12 07:37:28 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints numbers from n to 98
+ *
+ * Description: This function prints all the numbers from the given integer `n`
+ * to 98, separated by commas and followed by a new line.
+ *
+ * @n: The starting number
+ */
+
+void print_to_98(int n)
+{
+int num;
+if (n < 98)
+{
+for (num = n ; num <= 98 ; num++)
+{
+printf("%d", num);
+if (num != 98)
+{
+printf(",");
+printf(" ");
+}
+}
+}
+else if (n == 98)
+{
+printf("%d", n);
+}
+else 
+{
+for (num = n ; num >= 98 ; num--)
+{
+printf("%d", num);
+if (num != 98)
+{
+printf(",");
+printf(" ");
+}
+}
+}
+printf("\n");
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
 
