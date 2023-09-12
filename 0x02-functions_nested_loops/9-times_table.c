dbc0fcd4f1061ec2c5059f3c36ab833dbc6ec2e6
Meriem1551 <meriem200409@gmail.com>
2023-09-12 06:37:25 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - Prints the multiplication table for 0 to 9
+ *
+ * Description: This function prints the multiplication table for numbers from
+ * 0 to 9 in a specific format.
+ */
+void times_table(void)
+{
+int num1, num2, result;
+for (num1 = 0 ; num1 <= 9 ; num1++)
+{
+for (num2 = 0 ; num2 <= 9 ; num2++)
+{
+result = num1 * num2;
+_putchar(result + '0');
+if (num1 != 9 || num2 != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
