c06c8c2d6535dca8c19a4dadeccb266e3828b926
Meriem1551 <meriem200409@gmail.com>
2023-09-12 08:20:41 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,26 +9,44 @@
  */
 void print_times_table(int n)
 {
-if (n >= 0 && n <= 15)
-{
-int i, j;
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-int result = i * j;
-if (j == 0)
-{
-_putchar(result + '0');
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar(result + '0');
-}
-}
-_putchar('\n');
-}
-}
+	int x, y, z;
+
+	if (n >= 0 && n <= 14)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
