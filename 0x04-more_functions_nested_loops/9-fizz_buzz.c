4bff87e282630a8b161ae885bead568c7f0e0896
Meriem1551 <meriem200409@gmail.com>
2023-09-14 05:37:40 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,17 +16,17 @@ int main(void)
 int num;
 for (num = 1 ; num <= 100 ; num++)
 {
-if (num % 3 == 0 && num % 5 != 0)
+if (num % 3 == 0 && num % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (num % 3 == 0 && num % 5 != 0)
 {
 printf("Fizz");
 }
 else if (num % 5 == 0 && num % 3 != 0)
 {
 printf("Buzz");
-}
-else if (num % 3 == 0 && num % 5 == 0)
-{
-printf("FizzBuzz");
 }
 else
 {
