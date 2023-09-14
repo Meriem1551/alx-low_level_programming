001660bcadf41653c9e0df764ac4b17a32d7ffde
Meriem1551 <meriem200409@gmail.com>
2023-09-14 06:35:37 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_triangle - Prints a right-angled triangle of a specified size
+ *
+ * Description: This function prints a right-angled triangle composed of `#`
+ * characters. The size of the triangle is determined by the `size` parameter.
+ * The triangle is right-aligned. If `size` is 0 or less, nothing is printed.
+ *
+ * @size: The size of the triangle to print
+ */
+void print_triangle(int size)
+{
+int i, j;
+if (size <= 0)
+{
+printf("\n");
+}
+else
+{
+for (i = 1 ; i <= size ; i++)
+{
+for (j = 1 ; j < size-i ; j++)
+{
+printf(" ");
+}
+for (j = 1 ; j <= i ; j++)
+{
+printf("#");
+}
+printf("\n");
+}
+}
+}
