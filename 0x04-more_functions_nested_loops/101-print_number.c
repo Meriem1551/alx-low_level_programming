748afec4e86370e6cf70007cfbbea64b0e8d3cf2
Meriem1551 <meriem200409@gmail.com>
2023-09-14 07:18:46 -0700

    Numbers have life; they're not just symbols on paper

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer to the standard output
+ *
+ * Description: This function takes an integer as input and prints it to
+ * the standard output. It handles both positive and negative integers.
+ *
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+_putchar(n + '0');
+}
