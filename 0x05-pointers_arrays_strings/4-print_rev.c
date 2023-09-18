86b753f3bb4ae8af95a17c44592208a25758909f
Meriem1551 <meriem200409@gmail.com>
2023-09-18 04:31:29 -0700

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - Prints a string in reverse to the standard output
+ *
+ * Description:
+ * This function takes a pointer to a null-terminated string as input and
+ * prints the characters of the string in reverse order to the standard
+ * output. It starts from the end of the string and continues printing
+ * characters until it reaches the beginning.
+ *
+ * @s: A pointer to a null-terminated string
+ */
+void print_rev(char *s)
+{
+int index;
+for (index = strlen(s) ; index >= 0 ; index--)
+{
+_putchar(s[index]);
+}
+_putchar('\n');
+}
