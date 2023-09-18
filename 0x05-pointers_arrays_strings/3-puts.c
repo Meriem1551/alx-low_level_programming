0f7596e43677d3ce6827debb9402bdad42a1ca26
Meriem1551 <meriem200409@gmail.com>
2023-09-18 04:14:20 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -13,4 +13,5 @@
 void _puts(char *str)
 {
 puts(str);
+_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
