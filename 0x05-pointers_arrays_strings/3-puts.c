16aec87413204ea46ea0b9773dc44f215bbe6d8f
Meriem1551 <meriem200409@gmail.com>
2023-09-18 04:07:01 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string to the standard output
+ *
+ * Description:
+ * This function takes a pointer to a null-terminated string as input and
+ * prints the string to the standard output. It continues printing characters
+ * until it reaches the null terminator ('\0') at the end of the string.
+ *
+ * @str: A pointer to a null-terminated string
+ */
+void _puts(char *str)
+{
+puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
