272d5945a7197ec4a9bee26c4a8bf0663c47dd50
Meriem1551 <meriem200409@gmail.com>
2023-09-18 03:39:09 -0700

    Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,10 +2,10 @@
 /**
  * swap_int - Swaps the values of two integers.
  *
- * This file contains a C program that defines a 
+ * This file contains a C program that defines a
  * function to swap the values of
  * two integers. The function takes two integer pointers as parameters and
- * modifies the values they point to, effectively swapping 
+ * modifies the values they point to, effectively swapping
  * the values in memory.
  *
  * @a: A pointer to the first integer.
