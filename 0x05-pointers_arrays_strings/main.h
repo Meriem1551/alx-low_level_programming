25009865a3ac8a7c4e57500b71f2503eccbc3062
Meriem1551 <meriem200409@gmail.com>
2023-09-18 05:42:19 -0700

    strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,4 +7,4 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n);
+void print_array(int *a, int n);char *_strcpy(char *dest, char *src);
