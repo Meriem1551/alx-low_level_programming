c596ce374e76766b6511048178f4057f43828b3f
Meriem1551 <meriem200409@gmail.com>
2023-09-18 03:48:15 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - Calculates the length of a string.
+ *
+ * This file contains a C program that defines a function to calculate the
+ * length (number of characters) of a string. The function takes a pointer to a
+ * null-terminated string as a parameter and returns the length of the string.
+ *
+ * @s A pointer to a null-terminated string.
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
