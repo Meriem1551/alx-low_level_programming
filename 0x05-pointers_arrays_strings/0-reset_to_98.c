02309b408a5fb31b581e957e9d76d860dcb71a0e
Meriem1551 <meriem200409@gmail.com>
2023-09-18 03:08:09 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
  * value of the integer to 98. It effectively changes the value pointed to by
  * the pointer to 98.
  *
- * @n A pointer to an integer variable.
+ * @n: A pointer to an integer variable.
  */
 void reset_to_98(int *n)
 {
