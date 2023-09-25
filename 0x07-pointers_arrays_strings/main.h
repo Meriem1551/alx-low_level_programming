8b1ee4f2d70d309e502d24cec649a0b062a8c487
Meriem1551 <meriem200409@gmail.com>
2023-09-25 06:09:13 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ int _putchar(char ch);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
