a960811d8f7831b3dd71405b4a13b3715db0fa22
Meriem1551 <meriem200409@gmail.com>
2023-10-02 06:05:22 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,8 +1,19 @@
 #include <unistd.h>
 /**
- * main - function to print
+ * _putchar - print a character to the stabdard output
  *
- * Return: 0
+ * Description:
+ * This file contains the implementation of the _putchar function.
+ * The _putchar
+ * function is a custom character output function commonly used in C programs.
+ * It takes a single character as input and writes that character
+ * to the standard
+ * output (usually the console or terminal);
+ * @c: The character to be written to the standard output.
+ *
+ * Return:
+ * - On success, it returns the character that was written.
+ * - On error, it returns -1.
  */
 int _putchar(char c)
 {
