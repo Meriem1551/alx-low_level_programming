05ff077193d9fd6888f70c80358cbd28953355e1
Meriem1551 <meriem200409@gmail.com>
2023-10-07 05:40:23 -0700

    Fixing _calloc function issues

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,41 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
 /**
  * _calloc - allocates memory for an array
- * Description: allocates memory for an array, using malloc.
- * The _calloc function allocates memory for an array of nmemb elements
- * of size bytes each and returns a pointer to the allocated memory.
- * The memory is set to zero
  * @nmemb: number of elements in the array
  * @size: size of each element
- * Return: If nmemb or size is 0, then _calloc returns NULL
- * If malloc fails, then _calloc returns NULL.
+ *
+ * Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *c;
-c = malloc(nmemb * size);
-if (size == 0 || nmemb == 0)
-	return (NULL);
-
-if (c == NULL)
-	return (NULL);
-return (c);
+char *ptr;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+{
+return (NULL);
+}
+_memset(ptr, 0, nmemb * size);
+return (ptr);
 }
