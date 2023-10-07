804b7c65090e55b76d9221a014ce44870e2b4eea
Meriem1551 <meriem200409@gmail.com>
2023-10-07 05:57:43 -0700

    Fix some issues in _calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,8 @@
 #include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
-}
-/**
- * _calloc - allocates memory for an array
+ * *_calloc - allocates memory for an array
  * @nmemb: number of elements in the array
  * @size: size of each element
  *
@@ -26,16 +10,22 @@ return (s);
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *ptr;
+unsigned int i;
+char *p;
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
 }
-ptr = malloc(size * nmemb);
-if (ptr == NULL)
+p = malloc(nmemb * size);
+if (p == NULL)
 {
 return (NULL);
 }
-_memset(ptr, 0, nmemb * size);
-return (ptr);
+for (i = 0 ; i < nmemb * size ; i++)
+{
+p[i] = 0;
 }
+return (p);
+}
+
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,4 @@
 void *malloc_checked(unsigned int b);
 int _putchar(char ch);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-char *_memset(char *s, char b, unsigned int n)
 void *_calloc(unsigned int nmemb, unsigned int size);
