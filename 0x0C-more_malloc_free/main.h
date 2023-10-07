addce9bb830b8a70b8d0f92ec3d173d6c0681c5d
Meriem1551 <meriem200409@gmail.com>
2023-10-07 05:31:22 -0700

    Adding _memset func

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,5 @@
 void *malloc_checked(unsigned int b);
 int _putchar(char ch);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 void *_calloc(unsigned int nmemb, unsigned int size);
