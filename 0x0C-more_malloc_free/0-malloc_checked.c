492ff5dc54333ff350223e2f0080fac610ad4d69
Meriem1551 <meriem200409@gmail.com>
2023-10-07 02:30:05 -0700

    Fix betty style

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-	ptr = malloc(b);
-	if (ptr == NULL){
-		exit(98);
-	}
-	return (ptr);
+void *ptr;
+
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
 }
