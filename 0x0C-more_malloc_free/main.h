f5ec4b5c530190cbf93d8fdd1f83e9392c630a80
Meriem1551 <meriem200409@gmail.com>
2023-10-08 08:33:33 -0700

    Creating realloc function

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,4 +3,4 @@ int _putchar(char ch);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
