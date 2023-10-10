c9401f7e73179587ad6b24edc84cd779fb64fda1
Meriem1551 <meriem200409@gmail.com>
2023-10-10 10:42:16 -0700

    Adding betty style

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,20 +8,22 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-if (d->name == NULL)
-{
-printf("Name: (nil)\n");
-}
-else{
-printf("Name: %s\n", d->name);
-}
-printf("Age: %f\n", d->age);
-if (d->owner == NULL)
-{
-printf("Owner: (nil)");
-}
-else{
-printf("Owner: %s\n", d->owner);
-}
+	if (!(d->name))
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
+	if (!(d->owner))
+	{
+		printf("Owner: (nil)");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
 }
