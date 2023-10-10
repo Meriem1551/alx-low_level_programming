a62f6faddb1f1cd1b67e9efdf1dafce46f5f7697
Meriem1551 <meriem200409@gmail.com>
2023-10-10 11:15:38 -0700

    Declaring func to free the dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,5 @@ char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /*_dog_h_*/
