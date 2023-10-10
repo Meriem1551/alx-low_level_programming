9c11d3820b12e97cb82b0dc27cc9a8bb1115f26d
Meriem1551 <meriem200409@gmail.com>
2023-10-10 11:21:22 -0700

    Define func to free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog -  frees dogs.
+ * @d: dog's details
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
