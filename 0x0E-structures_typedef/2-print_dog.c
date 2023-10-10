8746357c1ed0ebfe18acbcea25eed8a80db74f10
Meriem1551 <meriem200409@gmail.com>
2023-10-10 10:28:56 -0700

    Creating func that print dog's details

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints a struct dog
+ * @d: dog's details(pointer)
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+else
+{
+if (d->name == NULL)
+{
+printf("Name: (nil)\n");
+return;
+}
+else if (d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+return;
+}
+else
+{
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
+}
+}
