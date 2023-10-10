d8e54010c761ecc7a1bcfaec04a854729ac40686
Meriem1551 <meriem200409@gmail.com>
2023-10-10 10:50:14 -0700

    Declaring func to create new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,5 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /*_dog_h_*/
