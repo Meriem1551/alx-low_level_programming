e4368ea87e973349db81a24dd87fa22d855266f4
Meriem1551 <meriem200409@gmail.com>
2023-10-10 09:57:46 -0700

    Creating struct named dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ * struct dog - such structure, wow
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif /*_dog_h_*/
