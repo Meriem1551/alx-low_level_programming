50c2ae79949b2bf3d85e9e83c777ee34dcc07cbb
Meriem1551 <meriem200409@gmail.com>
2023-10-11 10:52:20 -0700

    Creating main for task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/** main - main function of this code
+ * @argc: pointer to number of args
+ * @argv: pointer to the args
+ */
+int main (int argc, char *argv[])
+{
+int num1, num2;
+int result;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+if (argc > 4)
+{
+printf("Error\n");
+exit(98);
+}
+if (argv[2] != '+' || argv[2] != '-' || argv[2] != '*' || argv[2] != '/' || argv[2] != '%')
+{
+printf("Error\n");
+exit(99);
+}
+if (num2 == 0 && (argv[2] == '/' || argv[2] == '%'))
+{
+printf("Error\n");
+exit(100);
+}
+int (*operation_func)(int, int) = get_op_func(argv[2]);
+result = operation_func(num1, num2);
+printf("%d\n", result);
+return (0);
+}
