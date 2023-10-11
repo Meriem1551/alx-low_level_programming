f4aea73dc3aa9cec9d0973c8513b288919f13453
Meriem1551 <meriem200409@gmail.com>
2023-10-11 08:57:19 -0700

    Defining  print_name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * Description: this function takes a pointer to a string and a pointer
+ * to a function, and then call the function @f paasing the name a sparameter
+ *@name: a pointer to a string
+ *@f: a pointer to a function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
