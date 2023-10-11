871b3d4ff6e5443c880cdad7639292a72f3fb068
Meriem1551 <meriem200409@gmail.com>
2023-10-11 09:25:33 -0700

    Declaring func for task 2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,4 +4,5 @@
 int _putchar(char ch);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /* _function_pointers_h_*/
