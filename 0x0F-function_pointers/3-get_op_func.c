18ec779a50973474b76a4db5630004ac3291bb17
Meriem1551 <meriem200409@gmail.com>
2023-10-11 10:31:46 -0700

    Choosing the operator

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * get_op_func -  perform the operation asked by the user
+ * Description: takes an operator and return a pointer to the matched funtion
+ * @s: a pointer to the operator
+ * Return: a pointer to the function that corresponds to
+ * the operator given as a parameter.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+i = 0;
+while (i < 5)
+{
+if (s == ops[i]->op)
+{
+return (ops[i]->f);
+}
+i++;
+}
+return (NULL);
+}
