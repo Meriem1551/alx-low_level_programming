29d1a7b0bcd3092621666583d10c852f4096066e
Meriem1551 <meriem200409@gmail.com>
2023-10-11 09:49:40 -0700

    Fixing some errors

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-unsigned int i;
+int i;
 if (array == NULL || cmp == NULL)
 {
 return (0);
@@ -23,9 +23,9 @@ if (size <= 0)
 {
 return (-1);
 }
-for (i = 0 ; i <size ; i++)
+for (i = 0 ; i < size ; i++)
 {
-if (cpm(array[i]))
+if (cmp(array[i]))
 {
 return (i);
 }
