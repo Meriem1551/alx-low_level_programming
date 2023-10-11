be58096ea4d9da15b0bd5802a74c383dd101b1c7
Meriem1551 <meriem200409@gmail.com>
2023-10-11 09:21:57 -0700

    Defining the function for task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - executes a function given as a parameter on
+ * each element of an array
+ * DEscription: this function that takes an arraya and its size and executes
+ * a given func
+ * @array: a pointer to an array
+ * @size: is the size of the array
+ * @action:  is a pointer to the function you need to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int)){
+unsigned int i;
+if (array == NULL || action == NULL)
+{
+return;
+}
+for (i = 0 ; i < size ; i++)
+{
+action(array[i]);
+}
+}
