d771d8a5d9bf13434955501b8947bbb5c2e4ac48
Meriem1551 <meriem200409@gmail.com>
2023-10-11 09:46:40 -0700

    Creating func for task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - searches for an integer.
+ * Description: this function takes and array and its size and
+ * a function to compare intergers
+ * @array: a pointer to an array
+ * @size: is the number of elements in the array array
+ * @cmp: is a pointer to the function to be used to compare values
+ * Return:  returns the index of the first element for which the cmp
+ * function does not return 0
+ * if no element matches, return -1
+ * if size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+unsigned int i;
+if (array == NULL || cmp == NULL)
+{
+return (0);
+}
+if (size <= 0)
+{
+return (-1);
+}
+for (i = 0 ; i <size ; i++)
+{
+if (cpm(array[i]))
+{
+return (i);
+}
+}
+return (-1);
+}
+
