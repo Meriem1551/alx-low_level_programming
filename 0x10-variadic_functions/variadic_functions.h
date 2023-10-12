3abee34384dd90617785d4ac8ae1b274dc1c38d7
Meriem1551 <meriem200409@gmail.com>
2023-10-12 08:03:26 -0700

    Declaring task 3's func

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 void print_string(va_list);
 void print_char(va_list);
 void print_integer(va_list);
