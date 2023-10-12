a6e2cebb7caa16d5a36c457378ad5b7de8f961ab
Meriem1551 <meriem200409@gmail.com>
2023-10-12 07:03:47 -0700

    Declaring a struct and some funcs

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,23 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
+void print_string(va_list);
+void print_char(va_list);
+void print_integer(va_list);
+void print_float(va_list);
+
+
+/**
+ * struct opt - Typedef struct
+ *
+ * @op: Format
+ * @f: The function associated
+ **/
+typedef struct opt
+{
+	char *op;
+	void (*f)(va_list);
+} p_opt;
 #endif
