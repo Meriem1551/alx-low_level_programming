9a1185788922b0326a80f6551c4238b0d22e26df
Meriem1551 <meriem200409@gmail.com>
2023-10-12 07:53:38 -0700

    Declaring task 2's func

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_string(va_list);
 void print_char(va_list);
 void print_integer(va_list);
