31b7cabdb1c3d5ef0c75a8c35822df31f2a72847
Meriem1551 <meriem200409@gmail.com>
2023-10-19 12:16:21 -0700

    Fixing betty errors

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,10 @@
  * print_before_main - prints a string before main is executed
  * Return: void
  */
-void print_before_main() __attribute__((constructor));
+void print_before_main(void) __attribute__((constructor));
 
-void print_before_main(){
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI
+			bore my house upon my back!\n");
 }
