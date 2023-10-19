d7d3e4ec49abe0d9b5b0feb9d169a90fa59ea8b0
Meriem1551 <meriem200409@gmail.com>
2023-10-19 10:20:22 -0700

    Declaring task 2's func

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,5 +20,6 @@ struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 int _putchar(char c);
 #endif
