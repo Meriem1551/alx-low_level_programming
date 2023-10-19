5e1ea7e73d5cbf7046e464fcc8c29ca76f1e5f73
Meriem1551 <meriem200409@gmail.com>
2023-10-19 12:20:17 -0700

    Fixing some issues

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void print_before_main(void) __attribute__((constructor));
 
 void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI
-			bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-printf("(A tortoise, having pretty good sense
-of a hare's nature, challenges one to a race.)\n");
+printf("(A tortoise, having pretty good sense");
+printf("of a hare's nature, challenges one to a race.)\n");
 return (0);
 }
