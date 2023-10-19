cb7fbda7c969f60badf87b94f18f2d97a77a4209
Meriem1551 <meriem200409@gmail.com>
2023-10-19 09:06:36 -0700

    Adding lists.h to include

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - function print  all the elements of a list_t list.
+ * Description: this funtion take the header of a list as a param and traverse
+ * this lst to print its content
+ * @h: pointer to the first node
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	list_t *p = h;
+	unsigned int num_nodes = 0;
+	if (p == NULL)
+	{
+		printf("Empty list");
+		return (0);
+	}
+	while (p ! = NULL)
+	{
+		if (p->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s",p->len,  p->str);
+		}
+		num_nodes++;
+	}
+	return (num_nodes);
+}
