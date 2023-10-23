c55a827700d96a38fe464c98f4e95157dd0fdb77
Meriem1551 <meriem200409@gmail.com>
2023-10-22 22:57:16 -0700

    Defining a func for task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * add_nodeint - add anew node at the beginning of a list
+ * @head: pointer to a pointer to a list
+ * @n: data to add
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr = (listint_t *)malloc(sizeof(listint_t));
+	
+	if (ptr == NULL)
+		return NULL;
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+	return (*head);
+}
+
