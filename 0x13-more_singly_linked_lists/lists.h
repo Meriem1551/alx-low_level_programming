b015fb6bfbad5d8baf18f287734e2ab241c8f522
Meriem1551 <meriem200409@gmail.com>
2023-10-24 12:56:08 -0700

    Fixing code errors

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,6 +14,19 @@ typedef struct listint_s
 int n;
 struct listint_s *next;
 } listint_t;
+/**
+ * struct listnode_s - singly linked list
+ * @ptr: address of listint_t
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listnode_s
+{
+listint_t *ptr;
+struct listnode_s *next;
+} listnode_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
