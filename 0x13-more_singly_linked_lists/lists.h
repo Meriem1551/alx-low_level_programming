b8668439b4241ad97260eb886c804a6412f0f094
Meriem1551 <meriem200409@gmail.com>
2023-10-24 12:51:53 -0700

    Adding some secondary funcs

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,4 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+listnode_t *add_nodeptr(listnode_t **head, const listint_t *ptr);
+void free_listnode(listnode_t *head);
+int is_in_nodes(listnode_t *head, const listint_t *ptr);
 #endif
