2a9b96c96c3e3b18a5d26be818e10b85cd01dec6
Meriem1551 <meriem200409@gmail.com>
2023-10-24 11:31:55 -0700

    Fixing code errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	if (idx == 0)
 	{
-		new->newt = *head;
+		new->next = *head;
 		*head = new;
 		return (new);
 	}
