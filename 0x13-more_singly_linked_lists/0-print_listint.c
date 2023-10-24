19455bd8d93df76ea44e55f66cd080d0049062c4
Meriem1551 <meriem200409@gmail.com>
2023-10-23 22:29:04 -0700

    Fixing some errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,14 +1,15 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_list - prints all the elements of a list_t list
  * @h: constant pointer type list_t
  *
  * Return: the numbers of nodes
  */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes;
-	const list_t *p = h;
+	const listint_t *p = h;
 
 	num_nodes = 0;
 	while (p != NULL)
