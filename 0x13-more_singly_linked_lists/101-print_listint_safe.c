f58536c4a1dd942432d44c987617b2178e970385
Meriem1551 <meriem200409@gmail.com>
2023-10-24 12:58:59 -0700

    Fixing betty errors

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,6 +4,10 @@
 /**
  * print_linsint_safe -  prints a listint_t linked list.
  * @head: a pointer to te first node
+ * Description: This function can print lists with a loop
+ * You should go through the list only once
+ * If the function fails, exit the program with status 98
+ * Output format: see example
  * Returns: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
