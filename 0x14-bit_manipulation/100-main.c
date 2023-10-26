752237989269bf3126291bf42221ddf08d526fdb
Meriem1551 <meriem200409@gmail.com>
2023-10-26 10:49:51 -0700

    Creating func for task 6

---
+++ 0x14-bit_manipulation/100-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{
+int n;
+n = get_endianness();
+if (n != 0)
+{
+printf("Little Endian\n");
+}
+else
+{
+printf("Big Endian\n");
+}
+return (0);
+}
