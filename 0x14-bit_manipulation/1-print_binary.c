5f94b2e57c1ccd91316e520a1d86ab9c06b487b1
Meriem1551 <meriem200409@gmail.com>
2023-10-26 10:03:38 -0700

    Creating func for  task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_binary -  prints the binary representation of a number.
+ * @n: number in base 10
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 /*All function declaration*/
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif
