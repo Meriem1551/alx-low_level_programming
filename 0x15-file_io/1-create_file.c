25de041b9abcfec848bb9e409aa6beb3043e97a0
Meriem1551 <meriem200409@gmail.com>
2023-10-30 11:49:58 -0700

    Creating function for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_file:  creates a file.
+ * @filename: pointer
+ * @text_content: pointer to string
+ * Return: Returns: 1 on success, -1 on failure
+ * (file can not be created, file can not be written)
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ */
+int create_file(const char *filename, char *text_content)
+{
+int i = 0, file;
+
+if (filename == NULL)
+	return (-1);
+if (text_content == NULL)
+	text_content = "";
+while (text_content[i] != '\0')
+	i++;
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (file == -1)
+return (-1);
+write(file, text_content, i);
+return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,4 +6,5 @@
 #include <unistd.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
