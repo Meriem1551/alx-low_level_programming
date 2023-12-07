ebda02b7a2bacf2fc77d736b1d28447ba625b645
Meriem1551 <meriem200409@gmail.com>
2023-12-07 05:44:30 -0800

    solving task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,17 +9,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *tmp;
-	size_t num_nodes = 0;
-
-	if(h == NULL)
-		return (0);
-	tmp = h;
-	while(tmp != NULL)
-	{
-		printf("%d\n",tmp->n);
-	       	num_nodes++;
-       		tmp = tmp->next;
-	}
-	return (num_nodes);
+const dlistint_t *tmp;
+size_t num_nodes = 0;
+if(h == NULL)
+return (0);
+tmp = h;
+while(tmp != NULL)
+{
+printf("%d\n",tmp->n);
+num_nodes++;
+tmp = tmp->next;
+}
+return (num_nodes);
 }
